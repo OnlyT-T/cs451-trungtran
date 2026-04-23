d678b863f22b45ad32962c33e98161f722465ca1
OnlyT-T <trungonlyt145@gmail.com>
2026-04-22 22:55:54 -0400

    First upload folder from kali to github

---
+++ index1.php
@@ -0,0 +1,8 @@
+<html>
+	<body>
+		<h4>Hello World!</h4>
+	</body>
+	<?php
+		print("Hello PHP")
+	?>
+</html>
---
+++ index2.php
@@ -0,0 +1,11 @@
+<html>
+	<body>
+		<h1>Hu hu hu</h1>
+	</body>
+	<?php
+		ini_set('display_errors', '1');
+		init_set('display_startup_errors', '1');
+		error_reporting(E_ALL);
+		$conn = mysqli("localhost", "trungtran", "123", "lab_db");
+	?>
+</html>
